85ac558ec7fd789e4c0237752d8d1e2155fbd7d6
Yuesong <liuyuesongde@163.com>
2020-06-03 13:43:41 +0800

    websocket playground

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,6 +1,7 @@
 package websocket
 
 import (
+	"html/template"
 	"net/http"
 	"sync"
 
@@ -93,6 +94,91 @@ func (ws *WS) HTTPHandler(w http.ResponseWriter, r *http.Request) {
 	conn.Init()
 
 	defer ws.destroyConn(ws.id)
+}
+
+var page = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.innerHTML = message;
+        output.appendChild(d);
+    };
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        var addr = document.getElementById("addr").value
+        ws = new WebSocket(addr);
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server, 
+"Send" to send a message to the server and "Close" to close the connection. 
+You can change the message and send multiple times.
+<p>
+<form>
+<p>addr: 
+<input id="addr" type="text" value="{{.}}">
+</p>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output"></div>
+</td></tr></table>
+</body>
+</html>
+`))
+
+func (ws *WS) Playground(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	err := page.Execute(w, "ws://"+r.Host+"/ws")
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Receive 获取接收数据的 chan
--- websocket/websocket_test.go
+++ websocket/websocket_test.go
@@ -3,7 +3,6 @@ package websocket
 import (
 	"net/http"
 	"testing"
-	"text/template"
 )
 
 func TestWS(t *testing.T) {
@@ -12,9 +11,7 @@ func TestWS(t *testing.T) {
 	})
 
 	http.HandleFunc("/ws", ws.HTTPHandler)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		homeTemplate.Execute(w, "ws://"+r.Host+"/ws")
-	})
+	http.HandleFunc("/", ws.Playground)
 
 	go func() {
 		rec := ws.Receive()
@@ -27,80 +24,3 @@ func TestWS(t *testing.T) {
 	// 浏览器打开 http://127.0.0.1:8800 测试
 	t.Fatal(http.ListenAndServe("127.0.0.1:8800", nil))
 }
-
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.innerHTML = message;
-        output.appendChild(d);
-    };
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        var addr = document.getElementById("addr").value
-        ws = new WebSocket(addr);
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<p>addr: 
-<input id="addr" type="text" value="{{.}}">
-</p>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output"></div>
-</td></tr></table>
-</body>
-</html>
-`))
