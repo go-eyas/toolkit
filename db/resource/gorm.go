a319cd1e342b4ce6860b348336ec4162672bf735
isinged <isinged@163.com>
2020-06-07 00:13:26 +0800

    parse resource

--- db/resource/gorm.go
+++ db/resource/gorm.go
@@ -3,7 +3,20 @@ package resource
 import (
 	"github.com/jinzhu/gorm"
 	"net/http"
+	"strings"
 )
+
+type Field struct {
+	ColumnName   string // 数据库列名
+	StructKey    string // 结构体的名字
+	isPrimaryKey bool   // 是否主键
+	isIgnore     bool   // 该字段是否忽略
+	JsonKey      string // json key
+	Search       string // 查询方式
+	Order        string // 排序方式
+	Update       bool   // 是否可更新，默认不可以
+	Create       bool   // 是否可在创建时指定
+}
 
 type Resource struct {
 	db        *gorm.DB
@@ -12,18 +25,85 @@ type Resource struct {
 	scope     *gorm.Scope
 	pk        string
 	sample    interface{}
+	Fields    []*Field
+}
+
+func parseFields(scope *gorm.Scope) ([]*Field, string) {
+	gormFields := scope.Fields()
+	fields := make([]*Field, len(gormFields))
+
+	pk := scope.PrimaryKey()
+
+	for i, f := range gormFields {
+		field := &Field{
+			// 默认值
+			StructKey: f.Name,
+			ColumnName:   f.DBName,
+			JsonKey:      f.Name,
+			Search:       "=",
+			Order:        "DESC",
+			isPrimaryKey: pk == f.DBName,
+		}
+		fields[i] = field
+		tags := f.StructField.Tag
+		// json tag
+		jsonTag := tags.Get("json")
+		if jsonTag != "" {
+			field.JsonKey = jsonTag
+		} else if jsonTag == "-" {
+			field.JsonKey = ""
+		}
+
+		// parse resource tag
+		resourceTag := tags.Get("resource")
+
+		// 为空，全为默认值
+		if resourceTag == "" {
+			continue
+		}
+		// 忽略
+		if resourceTag == "-" {
+			field.isIgnore = true
+		}
+		tagList := strings.Split(resourceTag, ";")
+
+		for _, value := range tagList {
+			p := strings.Split(value, ":")
+			k := strings.TrimSpace(strings.ToUpper(p[0]))
+			v := ""
+			if len(p) >= 2 {
+				v = strings.Join(p[1:], ":")
+			}
+			switch k {
+			case "PK":
+				pk = field.ColumnName
+				field.isPrimaryKey = true
+			case "SEARCH":
+				field.Search = strings.ToUpper(v)
+			case "ORDER":
+				field.Order = strings.ToUpper(v)
+			case "CREATE":
+				field.Create = true
+			case "UPDATE":
+				field.Update = true
+			}
+		}
+
+	}
+	return fields, pk
 }
 
 func NewGormResource(db *gorm.DB, v interface{}) *Resource {
 	scope := db.NewScope(v)
-
+	fields, pk := parseFields(scope)
 	return &Resource{
 		sample:    v,
 		db:        db,
 		tableName: scope.TableName(),
 		model:     db.Table(scope.TableName()),
 		scope:     scope,
-		pk:        scope.PrimaryKey(),
+		pk:        pk,
+		Fields:    fields,
 	}
 }
 
@@ -37,7 +117,11 @@ func (r *Resource) Create(v interface{}) error {
 func (r *Resource) Update(pk interface{}, v interface{}) error {
 	return r.Row(pk).Updates(v).Error
 }
-func (r *Resource) Query(v interface{}, slice interface{}) (int64, error) {
+
+func (r *Resource) Detail(pk interface{}, v interface{}) error {
+	return r.Row(pk).First(v).Error
+}
+func (r *Resource) List(v interface{}, slice interface{}) (int64, error) {
 	return 0, nil
 }
 func (r *Resource) Delete(pk interface{}) error {
--- db/resource/gorm_test.go
+++ db/resource/gorm_test.go
@@ -2,50 +2,101 @@ package resource
 
 import (
   "github.com/go-eyas/toolkit/db"
+  "github.com/jinzhu/gorm"
   "testing"
 )
 
 type Article struct {
-  ID      int64  `resource:"pk;search:none"`
-  Title   string `resource:"create;update;search:like"`
-  Content string `resource:"create;update;search:like"`
-  Status  byte   `resource:"search:="`
+  ID      int64  `resource:"pk;search:=;order:desc" json:"id"`
+  Title   string `resource:"create;update;search:like" json:"title"`
+  Content string `resource:"create;update;search:like" json:"text"`
+  Status  byte   `resource:"search:=" json:"-"`
 }
 
-func TestGormResource(t *testing.T) {
+func testDB() *gorm.DB {
   DB, err := db.Gorm(&db.Config{
     Debug:  true,
     Driver: "mysql",
-    URI:    "root:123456@(10.0.2.252:3306)/test",
+    URI:    "root:eyas8825345liu@(111.230.219.41:3306)/toolkit_test",
   })
   if err != nil {
     panic(err)
   }
+  return DB
+}
+
+func TestCreate(t *testing.T) {
+  DB := testDB()
   r := NewGormResource(DB, Article{})
   DB.AutoMigrate(&Article{})
 
-  //   create
-  article := &Article{
-    Title:   "测试文章",
+  //   create by struct
+  err := r.Create(&Article{
+    Title:   "测试文章 origin",
     Content: "文章的内容",
-  }
-  err = r.Create(article)
-  if err != nil {
-    panic(err)
-  }
-
-  err = r.Update(article.ID, map[string]interface{}{
-    "content": "修改后的文章内容",
   })
-
   if err != nil {
     panic(err)
   }
 
-  err = r.Delete(article.ID)
-
+  // create by tmp struct
+  err = r.Create(&struct{
+    Title string
+    Content string
+  }{
+    Title:   "测试文章 tmp struct",
+    Content: "文章的内容",
+  })
   if err != nil {
     panic(err)
   }
+
+  // create by map
+  err = r.Create(map[string]interface{}{
+    "title":   "测试文章 map",
+    "content": "文章的内容",
+  })
+  if err != nil {
+    panic(err)
+  }
+
+  list := []*Article{}
+  total, err := r.List(struct{}{}, &list)
+  if err != nil {
+    panic(err)
+  }
+  t.Logf("total=%d list=%+v", total, list)
+
 
 }
+
+// func TestGormResource(t *testing.T) {
+//   DB := testDB()
+//   r := NewGormResource(DB, Article{})
+//   DB.AutoMigrate(&Article{})
+//
+//   //   create
+//   article := &Article{
+//     Title:   "测试文章",
+//     Content: "文章的内容",
+//   }
+//   err = r.Create(article)
+//   if err != nil {
+//     panic(err)
+//   }
+//
+//   err = r.Update(article.ID, map[string]interface{}{
+//     "content": "修改后的文章内容",
+//   })
+//
+//   if err != nil {
+//     panic(err)
+//   }
+//
+//   err = r.Delete(article.ID)
+//
+//   if err != nil {
+//     panic(err)
+//   }
+//
+// }
