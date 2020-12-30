7557119f68d76e342d2634876fd80680e5e7c6a9
liuyuesong <liuyuesongde@163.com>
2020-12-30 10:32:12 +0800

    resp comment

--- http/v2/response.go
+++ http/v2/response.go
@@ -53,6 +53,7 @@ func (rp *Response) ready() {
 	}
 }
 
+// StatusCode 获取 HTTP 响应状态码
 func (rp *Response) StatusCode() int {
 	if rp.Response == nil {
 		return 0
@@ -60,10 +61,12 @@ func (rp *Response) StatusCode() int {
 	return rp.Response.StatusCode
 }
 
+// Status StatusCode 别名
 func (rp *Response) Status() int {
 	return rp.StatusCode()
 }
 
+// GetError 获取错误
 func (rp *Response) GetError() error {
 	if len(rp.Err.errs) > 0 {
 		return rp.Err
@@ -71,10 +74,12 @@ func (rp *Response) GetError() error {
 	return nil
 }
 
+// AddError 手动增加错误
 func (rp *Response) AddError(err error) {
 	rp.Err.Add(err)
 }
 
+// ReadAllBody 读取响应的 Body 流
 func (rp *Response) ReadAllBody() (bt []byte, err error) {
 	if rp.Response != nil && !rp.IsRead {
 		bt, err = ioutil.ReadAll(rp.Response.Body)
@@ -86,20 +91,30 @@ func (rp *Response) ReadAllBody() (bt []byte, err error) {
 	return
 }
 
+// Body 获取响应的原始数据
 func (rp *Response) Body() (bt []byte) {
 	bt, _ = rp.ReadAllBody()
 	return
 }
 
+// SetBody 重置响应的 Body
+func (rp *Response) SetBody(bt []byte) {
+	rp.IsRead = true
+	rp.body = bt
+}
+
+// String 将响应的 Body 数据转成字符串
 func (rp *Response) String() string {
 	if !rp.IsRead { rp.ReadAllBody() }
 	return string(rp.Body())
 }
 
+// Error 实现 error interface
 func (rp *Response) Error() string {
 	return rp.Err.Error()
 }
 
+// JSON 使用 JSON 解析响应 Body 数据
 func (rp *Response) JSON(v interface{}) error {
 	if !rp.IsRead { rp.ReadAllBody() }
 	return json.Unmarshal(rp.body, v)
