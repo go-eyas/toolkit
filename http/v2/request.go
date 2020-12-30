d8dd9f16363904da598f87bc9f388950e7173628
liuyuesong <liuyuesongde@163.com>
2020-12-30 17:58:15 +0800

    fix mdl add error

--- http/v2/request.go
+++ http/v2/request.go
@@ -370,6 +370,7 @@ func (c *Client) DoRequest(method, u string, data ...interface{}) (resp *Respons
 		for _, h := range cli.resMdls {
 			resp = h(cli, httpReq, resp)
 		}
+		err = resp.GetError()
 	}()
 
 	for {
